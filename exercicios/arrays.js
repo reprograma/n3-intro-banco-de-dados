0098b9220939dc90a57c6b6ec2eee1c5acfdf33c
Leticia Tiveron <leticiativeronbt@gmail.com>
2019-11-14 22:15:52 -0300

    Exercicios de array

--- exercicios/arrays.js
+++ exercicios/arrays.js
@@ -6,11 +6,19 @@ const comidas = [
 ]
 
 // 01: Selecione apenas a batatinha
+const batatinha = comidas.find(c => c === "Batata frita")
+console.log('*** batatinha:', batatinha)
 
 // 02: Selecione apenas as comidas que comecem com `s` ou `b`
+const comidasComSOuB = comidas.filter(c => (/^s|^b/i).test(c))
+console.log('*** comidasComSOuB:', comidasComSOuB)
 
-// 03: transforme a lista de comidas em comidas divertidas (adicione uma carinha feliz nelas)
+// 03: transforme a lista de comidas em comidas
+// divertidas (adicione uma carinha feliz nelas)
+const comidasDivertidas = comidas.map(c => c + ' :D o/ sextou ^.^')
+console.log('*** comidasDivertidas:', comidasDivertidas)
 
+// --------------------
 const comidas2 = [
   { nome: 'Batata frita', vegano: true },
   { nome: 'Hamburguer', vegano: false },
@@ -21,3 +29,13 @@ const comidas2 = [
 // 04: Selecione apenas as comidinhas veganas.
 
 // 05: Selecione apenas a primeira comidinhas vegana do cardápio.
+
+// --------------------
+const comidas3 = [
+  { nome: 'Batata frita', vegano: true, preco: 10.90 },
+  { nome: 'Hamburguer', vegano: false, preco: 19.80 },
+  { nome: 'Sorvete de chocolate', vegano: false, preco: 7 },
+  { nome: 'Saladinha delicia', vegano: true, preco: 12 }
+]
+
+// 06: Calcule o valor total desse cardapio
